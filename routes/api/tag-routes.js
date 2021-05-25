30f5e6ee34b8188a71658b7cb2d02d970a19bdc8
mythosmystery <axiysfire@gmail.com>
2021-05-25 01:21:47 -0500

    added post, put and delte routes to tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,14 +31,44 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tag = await Tag.create(req.body);
+    res.status(200).json(req.body);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    category ? res.status(200).json(tag) : res.status(404).json({ message: "No category with this ID!" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    category ? res.status(200).json(tag) : res.status(404).json({ message: "No category with this ID!" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
